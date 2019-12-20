0e60f2fc2875eb9d8b2ebe304d584cb5e758e462
maxyang2008 <maxyang2008@163.com>
2019-12-20 09:29:47 +0800

    Create linux6最小安装时正确安装vnc.sh

---
+++ linux/linux6最小安装时正确安装vnc.sh
@@ -0,0 +1,7 @@
+yum install gnome-panel
+yum install gnome-terminal
+yum install xorg-x11-xinit
+yum install xorg-x11-fonts-Type1
+yum install xorg-x11-font*
+yum install dejavu-sans-fonts
+yum install dejavu-sans-mono-fonts
